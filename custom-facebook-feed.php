e79575d6eb40dac3031fc17760a230e5d438bdfc
domabo.com <admin@domabo.com>
2014-06-14 17:13:25 -0500

    Update custom-facebook-feed.php

--- custom-facebook-feed.php
+++ custom-facebook-feed.php
@@ -1024,8 +1024,13 @@ function display_cff($atts) {
                         
                         //Show event details
                         if ($cff_show_event_details){
-                            //Location
-                            if (!empty($event_object->location)) $cff_event .= '<p class="cff-where" ' . $cff_event_details_styles . '>' . $event_object->location . '</p>';
+                            
+                               //Location
+                        if (!empty($event_object->location)) 
+                         {
+                                 $cff_event .= '<p class="cff-where" ' . $cff_event_details_styles . '><a href="https://facebook.com/'. $event_object->venue->id .'">' . $event_object->location . '</a></p>';
+                          }
+
                             //Description
                             if (!empty($event_object->description)){
                                 $description = $event_object->description;
